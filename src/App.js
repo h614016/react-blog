ba4edf3819ebbb32310591842db779e80edda020
h614016 <h614016@naver.com>
2022-05-22 20:01:54 +0900

    'scss설치'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './logo.svg';// eslint-disable-line no-unused-vars
+import './App.scss'
 
 function App() {
+  let posts = '강남 고기 맛집'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className='list'>
+        <h3>{posts}</h3>
+        <p>2월 17일 발행</p>
+        <hr /> {/* 수평선 */}
+      </div>
     </div>
   );
 }
