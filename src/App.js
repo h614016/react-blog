63ef2b6bb617fb47c38a1dd21c5505c9f4079682
h614016 <h614016@naver.com>
2022-05-22 21:16:49 +0900

    '5강까지'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';// eslint-disable-line no-unused-vars
-import './App.scss'
+/* eslint-disable */
+
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-  let posts = '강남 고기 맛집'
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "남자 자켓 추천",
+    "남자 이너 추천",
+  ]);
+
+  let [따봉, 따봉변경] = useState(0);
+  function 여자코트추천() {
+    let 새로운글제목 = [...글제목];
+
+    if ((글제목[0] = "남자 코트 추천")) {
+      새로운글제목[0] = "여자 코트 추천";
+      글제목변경(새로운글제목);
+    }
+  }
+  function 따봉업() {
+    따봉변경(따봉 + 1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className='list'>
-        <h3>{posts}</h3>
+      <div className="list">
+        <h3>
+          {글제목[0]} <span onClick={따봉업}>👍{따봉}</span>
+        </h3>
+        <button onClick={여자코트추천}>글제목 변경</button>
+        <p>2월 17일 발행</p>
+        <hr /> {/* 수평선 */}
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr /> {/* 수평선 */}
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
         <p>2월 17일 발행</p>
         <hr /> {/* 수평선 */}
       </div>
